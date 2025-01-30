416dc8f4bb07513549d20b8e9f3248ca3afa10d9
Joseph Pal <josephpal27@gmail.com>
2025-01-30 15:40:40 +0530

    implement ToDo app with add and delete functionality; update dependencies and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useState } from 'react';
 import './App.css'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [todoList, setTodoList] = useState([]);
+
+  function submitTodo(e) {
+    e.preventDefault();
+    let todo = e.target.todo.value.trim();
+
+    if (!todoList.includes(todo) && todo !== '') {
+      let finalList = [...todoList, todo];
+      setTodoList(finalList);
+      toast.success('ToDo Added Successfully!', { autoClose: 3000 });
+    } else if (todo == '') {
+      // alert('Please Enter Any ToDo')
+      toast.info('Please Enter Any ToDo!', { autoClose: 3000 });
+    } else {
+      // alert('ToDo Already Added')
+      toast.warning('ToDo Already Added!', { autoClose: 3000 });
+    }
+
+    // to delete the input value after todo added
+    e.target.todo.value = '';
+  }
+
+  let item = todoList.map((value, index) => {
+    return (
+      <List 
+      key={index} 
+      value={value}
+      todoList={todoList}
+      ind={index}
+      setTodoList={setTodoList}
+      />
+    )
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="todo-app">
+        <h1>ToDo App</h1>
+
+        <form onSubmit={submitTodo}>
+          <input type="text" name='todo' />
+          <button>ADD</button>
+        </form>
+
+        <div className="todo-list">
+          <ul>
+            {item}
+          </ul>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* For Notification */}
+      <ToastContainer />
     </>
   )
 }
 
 export default App
+
+
+function List({value, todoList, ind, setTodoList}) {
+
+  let deleteTodo = () => {
+    let finalTodoList = todoList.filter((v, i) => i !== ind);  
+    setTodoList(finalTodoList);
+    toast.error('ToDo Deleted Successfully!', { autoClose: 2000 }); 
+  }
+
+  return (
+    <li>{value} <span onClick={deleteTodo}>&times;</span></li>
+  )
+}
